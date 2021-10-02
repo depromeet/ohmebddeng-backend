773c06a56cc7eac8e72742f634e9e955d28b93b2
devsungmin <kimsungmin.dev@gmail.com>
2021-10-02 18:20:37 +0900

    fix: 🐛 package에러 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { APP_PIPE } from '@nestjs/core';
-=======
 import { TypeOrmModule } from '@nestjs/typeorm';
 
->>>>>>> feat-#3
 @Module({
   imports: [TypeOrmModule.forRoot()], // ormconfig.json값을 가져옴
   controllers: [AppController],
