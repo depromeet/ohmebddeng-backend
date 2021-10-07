6ac7a72d5b23eed4e2bdf2be18f9755b87921e3a
devsungmin <kimsungmin.dev@gmail.com>
2021-10-07 20:57:42 +0900

    fix: 🐛 food, restaurant DTO 수정
    ✅ Closes: #10

--- src/food/dto/create-food.dto.ts
+++ src/food/dto/create-food.dto.ts
@@ -1,21 +1,6 @@
-import { IsBoolean, IsDate, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class CreateFoodDto {
   @IsString()
-  readonly id: string;
-
-  @IsString()
   readonly name: string;
-
-  @IsDate()
-  readonly createdAt: Date;
-
-  @IsDate()
-  readonly deletedAt: Date;
-
-  @IsBoolean()
-  readonly isDeleted: boolean;
-
-  @IsString()
-  readonly imageUrl: string;
 }
--- src/food/dto/create-restaurant.dto.ts
+++ src/food/dto/create-restaurant.dto.ts
@@ -2,41 +2,5 @@ import { IsBoolean, IsNumber, IsString, IsDate } from 'class-validator';
 
 export class CreateRestaurantDto {
   @IsString()
-  id: string;
-
-  @IsString()
   name: string;
-
-  @IsDate()
-  createdAt: Date;
-
-  @IsDate()
-  updatedAt: Date;
-
-  @IsDate()
-  deletedAt: Date;
-
-  @IsBoolean()
-  isDeleted: boolean;
-
-  @IsString()
-  phoneNumber: string;
-
-  @IsString()
-  address: string;
-
-  @IsNumber()
-  mapX: number;
-
-  @IsNumber()
-  mapY: number;
-
-  @IsString()
-  naverMapUrl: string;
-
-  @IsString()
-  kakaoMapUrl: string;
-
-  @IsString()
-  imageUrl: string;
 }
