b5edb75ebc7ddeee1f2d8d2c58a4f76ea7db420a
Kong Chae Won <chaewonkong@gmail.com>
2021-10-17 15:25:30 +0900

    docs: ✏️ swagger를 dto, entity, controller에 추가합니다

--- src/user/entities/user_level.entity.ts
+++ src/user/entities/user_level.entity.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class UserLevel {
   @PrimaryGeneratedColumn({ type: 'bigint' })
+  @ApiProperty({ description: '사용자 레벨 ID' })
   id: string;
 
   @Column()
+  @ApiProperty({ description: '사용자 레벨 이름' })
   name: string;
 
   @Column({ nullable: true })
+  @ApiProperty({ description: '사용자 레벨 이미지', nullable: true })
   imageUrl: string;
 
   @Column({ nullable: true })
+  @ApiProperty({ description: '사용자 레벨 설명', nullable: true })
   description: string;
 
   @Column()
+  @ApiProperty({ description: '사용자 레벨' })
   level: number;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,18 @@ import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserLevelDto } from './dto/create-user-level.dto';
 import { GetUserLevelDto } from './dto/get-user-level.dto';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { HOT_LEVEL } from 'src/food/enums/hot-level';
+import { UserLevel } from './entities/user_level.entity';
 @Controller('user')
+@ApiTags('사용자 API')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -18,6 +29,15 @@ export class UserController {
   }
 
   @Post('level')
+  @ApiOperation({
+    summary: '사용자 레벨테스트 결과 제출 API',
+    description: `레벨테스트 응답결과를 제출하고 사용자의 레벨을 받는다. (참고) hotLevel =  "EASY" | "NORMAL" | "HOT" | "HOTTEST"`,
+  })
+  @ApiResponse({
+    description: '사용자의 ID와 레벨을 받는다',
+    status: 200,
+    type: GetUserLevelDto,
+  })
   async postUserLevel(
     @Body() params: CreateUserLevelDto,
   ): Promise<GetUserLevelDto> {
