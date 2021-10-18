450cf6ceb28ba4120ee56ebbfdeefca87b0e2a63
Kong Chae Won <chaewonkong@gmail.com>
2021-10-19 08:54:48 +0900

    refactor: 💡 불필요한 메서드를 제거하고 적절한 네이밍을 선택합니다

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,6 +11,11 @@ import {
 import { Food } from 'src/food/entities/food.entity';
 import { EvaluateUserLevel } from './utils/evaluate-user-level';
 import { GetUserLevelDto } from './dto/get-user-level.dto';
+
+interface IFoodLevel {
+  foodId: string;
+  foodLevelId: string;
+}
 @Injectable()
 export class UserService {
   constructor(
@@ -41,52 +46,40 @@ export class UserService {
     let userLevel = new UserLevel();
 
     const { userId, answers } = params;
+
     // get each food level
     const foodIds = answers.map(({ foodId }) => foodId);
-    const foodsFoundByIds = await this.foodRepository
+    const foods: IFoodLevel[] = await this.foodRepository
       .createQueryBuilder('food')
       .leftJoin('food.foodLevel', 'foodLevel')
       .select(['food.id', 'foodLevel.id'])
       .where('food.id IN (:...ids)', { ids: foodIds })
-      .getMany();
+      .getMany()
+      .then((foods) =>
+        foods.map(({ id: foodId, foodLevel }) => ({
+          //foodId, foodLevelId만 뽑아낸다.
+          foodId,
+          foodLevelId: foodLevel.id,
+        })),
+      );
 
     /**
-     * Food를 받아 foodId와 foodLevelId를 객체로 하는 요소를 가지는 배열을 반환합니다.
-     * @param foods Food에서 id와 foodLevel만을 발라낸 배열입니다.
-     * @returns foodId와 foodLevelId의 배열을 반환합니다. {foodId, foodLevelId}[]
-     */
-    const generateArrayWithFoodIdAndFoodLevel = (
-      foods: Pick<Food, 'id' | 'foodLevel'>[],
-    ) => {
-      return foods.map(({ id, foodLevel }, idx) => ({
-        foodId: id,
-        foodLevelId: foodLevel.id,
-      }));
-    };
-
-    /**
+     * EvaluateLevel에 필요한 형태로 foods와 answers 두 배열을 가공합니다.
      * foods와 answers 두 배열을 합칩니다. 동일한 foodId를 기준으로 두 배열의 각 요소는 합쳐지게 됩니다. foodId는 제외합니다.
      * @param foods foodId와 foodLeveLid로 이뤄진 배열
      * @param answers foodId와 hotLevelId로 이뤄진 배열
      * @returns foods와 answers를 겹치는 foodId를 기준으로 요소를 합친 배열 { foodLevelId, hotLevelId }
      */
-    const mergeFoodsAndAnswersByFoodId = (
-      foods: { foodId: string; foodLevelId: string }[],
+    const createEvaluateUserLevelParam = (
+      foods: IFoodLevel[],
       answers: TemporaryAnswer[],
     ) => {
       return foods.map(({ foodId, foodLevelId }) => {
         // answers에서 food와 foodId가 같은 요소를 탐색
-        const matchedAnswer = answers.find(
-          (answer) => answer.foodId === foodId,
-        );
+        const { hotLevel } = answers.find((answer) => answer.foodId === foodId);
 
-        // 응답(answer)에 일치하는 foodId가 있는 경우에만 반환되는 배열에 추가
-        if (matchedAnswer) {
-          return {
-            hotLevel: matchedAnswer.hotLevel,
-            foodLevelId,
-          };
-        }
+        // 응답(answer)에 일치하는 foodId가 있는 경우만 hotLevel과 foodLevelId를 반환되는 배열에 추가
+        return hotLevel && { hotLevel, foodLevelId };
       });
     };
 
@@ -94,12 +87,12 @@ export class UserService {
     /**
      * foodLevelId, hotLevel로 이루어진 배열을 만듭니다
      */
-    const mergedAnswersWithFoodLevel = mergeFoodsAndAnswersByFoodId(
-      generateArrayWithFoodIdAndFoodLevel(foodsFoundByIds),
+    const evaluateUserLevelParams = createEvaluateUserLevelParam(
+      foods,
       answers,
     );
 
-    const evaluateUserLevel = new EvaluateUserLevel(mergedAnswersWithFoodLevel);
+    const evaluateUserLevel = new EvaluateUserLevel(evaluateUserLevelParams);
     userLevel.id = evaluateUserLevel.evaluateLevel().toString();
 
     // save userLevel in DB
