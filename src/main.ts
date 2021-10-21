14271b4875bd73a39120d044c734227dea0dd3a1
devsungmin <kimsungmin.dev@gmail.com>
2021-10-21 22:49:51 +0900

    fix: 🐛 CORS 문제 해결
    ✅ Closes: #39

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = 3000;
 
   const prefix = '/v1';
