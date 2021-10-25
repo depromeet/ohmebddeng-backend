db6bf530aeb0a11cf4c35c3d7666eee3d39e23f7
Kong Chae Won <chaewonkong@gmail.com>
2021-10-25 23:50:56 +0900

    refactor: 💡 size, shuffle, hotLevel을 받도록 dto 수정합니다

--- src/food/dto/find-foods.dto.ts
+++ src/food/dto/find-foods.dto.ts
@@ -1,6 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import {
+  IsBooleanString,
+  IsEnum,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { HOT_LEVEL } from '../enums/hot-level';
 
+/**
+ * category: 가져오려는 음식의 category. Nullable.
+userId: 사용자의 Id. Nullable.
+foodLevel: 가져오려는 음식의 레벨(enum HOT_LEVEL). NOT Nullable.
+size: 가져올 음식의 개수. default === 10
+shuffle: 랜덤 여부. Boolean. default === true
+ */
 export class FindFoodsDto {
   @IsString()
   @IsOptional()
@@ -11,4 +25,18 @@ export class FindFoodsDto {
   @IsOptional()
   @ApiProperty({ description: '카테고리', required: false })
   category?: string;
+
+  @IsEnum(HOT_LEVEL)
+  @ApiProperty({ description: '가져오려는 음식의 맵기 레벨' })
+  hotLevel: HOT_LEVEL;
+
+  @IsNumberString()
+  @IsOptional()
+  @ApiProperty({ description: '가져오려는 음식의 개수', required: false })
+  size: string;
+
+  @IsBooleanString()
+  @IsOptional()
+  @ApiProperty({ description: '가져오려는 음식의 개수', required: false })
+  shuffle: string;
 }
