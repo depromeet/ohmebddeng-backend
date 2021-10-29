26fe8dfe42149277857b04428eca00fc212ac6e4
Kong Chae Won <chaewonkong@gmail.com>
2021-10-29 23:01:53 +0900

    fix: 🐛 reviewList swagger 타입 지정

--- src/review/dto/create-reviews.dto.ts
+++ src/review/dto/create-reviews.dto.ts
@@ -2,24 +2,29 @@ import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
 import { IsString, IsArray, IsEnum } from 'class-validator';
 import { HOT_LEVEL } from 'src/common/enums/hot-level';
 
-export class Review {
+export class ReviewDto {
   @IsEnum(HOT_LEVEL)
-  @ApiProperty({ description: "음식 레벨 ex) 'EASY', 'NORMAL', 'HOT', 'HOTTEST', 'NEVER_TRIED'"})
+  @ApiProperty({
+    description:
+      "음식 레벨 ex) 'EASY', 'NORMAL', 'HOT', 'HOTTEST', 'NEVER_TRIED'",
+  })
   hotLevel: HOT_LEVEL;
+
   @IsString()
-  @ApiProperty({ description: '음식 id'})
+  @ApiProperty({ description: '음식 id' })
   foodId: string;
-  @IsString({each: true})
-  @ApiProperty({ description: '음식 맛평가 태그 id'})
+
+  @IsString({ each: true })
+  @ApiProperty({ description: '음식 맛평가 태그 id' })
   tagIds: string[];
 }
 
 export class CreateReviewsDto {
   @IsString()
-  @ApiProperty({ description: '유저 id'})
+  @ApiProperty({ description: '유저 id' })
   userId: string;
 
   @IsArray()
-  @ApiProperty({ description: '리뷰 클래스의 배열'})
-  reviewList: Review[];
+  @ApiProperty({ description: '리뷰 클래스의 배열', type: [ReviewDto] })
+  reviewList: ReviewDto[];
 }
