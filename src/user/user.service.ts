a3af287b4fce8c29e24f65671e93f49880f69c67
Kong Chae Won <chaewonkong@gmail.com>
2021-11-13 14:28:09 +0900

    feat: 🎸 validateUser 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -37,6 +37,21 @@ export class UserService {
     const { anonymousId, id: userId } = await this.userRepository.save(user);
     return { anonymousId, userId };
   }
+
+  async validateUser(userId: string, anonymousId: string) {
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.userLevel', 'userLevel')
+      .leftJoinAndSelect('userLevel.userLevelDetail', 'userLevelDetail')
+      .where('user.id = :userId', { userId })
+      .andWhere('user.anonymousId = :anonymousId', { anonymousId })
+      .getOne();
+    if (!user) {
+      throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+
   /**
    * 사용자 id를 기반으로 해당 사용자의 정보를 가져오는 API
    * @param userId 사용자 id를 param으로 받음
@@ -46,6 +61,7 @@ export class UserService {
     userId: string,
     anonymousId: string,
   ): Promise<FindUserDto | Omit<FindUserDto, 'userLevel'>> {
+    await this.validateUser(userId, anonymousId);
     const query = this.userRepository
       .createQueryBuilder('user')
       .leftJoinAndSelect('user.userLevel', 'userLevel')
@@ -100,10 +116,16 @@ export class UserService {
       });
   }
 
-  async updateUserLevel(params: updateUserLevelDto): Promise<FindUserLevelDto> {
+  async updateUserLevel(
+    params: updateUserLevelDto,
+    anonymousId: string,
+  ): Promise<FindUserLevelDto> {
     let userLevel = new UserLevel();
 
     const { userId, answers } = params;
+
+    // validate user with anonymousId
+    await this.validateUser(userId, anonymousId);
 
     // get each food level
     const foodIds = answers.map(({ foodId }) => foodId);
