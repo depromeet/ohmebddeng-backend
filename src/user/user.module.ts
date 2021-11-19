206a79df69057deda949a2b113162df7940fefcf
Kong Chae Won <chaewonkong@gmail.com>
2021-11-19 11:02:24 +0900

    fix: 🐛 불필요한 import 제거

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,15 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserRefreshToken } from './entities/user_refresh_token.entity';
 import { Food } from 'src/food/entities/food.entity';
-import { TransformDto, TransformDtoModule } from './utils/transform.dto';
-import { TransformDao, TransformDaoModule } from './utils/transform.dao';
+import { TransformDto } from './utils/transform.dto';
+import { TransformDao } from './utils/transform.dao';
 
 @Module({
-  imports: [
-    // TransformDto,
-    // TransformDao,
-    TypeOrmModule.forFeature([User, UserRefreshToken, Food]),
-  ],
+  imports: [TypeOrmModule.forFeature([User, UserRefreshToken, Food])],
   controllers: [UserController],
   providers: [UserService, TransformDao, TransformDto],
 })
