9f9092ccd7feee7ccc3f86f547d2223dd9bc5a81
Kong Chae Won <chaewonkong@gmail.com>
2021-11-19 10:46:52 +0900

    refactor: 💡 transformDao, transformDto 생성

---
+++ src/user/utils/transform.dao.ts
@@ -0,0 +1,67 @@
+import { ERROR_MESSAGE } from '@common/enums/error-message';
+import { HttpException, HttpStatus, Module } from '@nestjs/common';
+import { User } from '@user/entities/user.entity';
+
+interface IFoodLevel {
+  foodId: string;
+  foodLevelId: string;
+}
+
+export class TransformDao {
+  findUser(user: User) {
+    try {
+      const { userLevel, isDeleted, role, ...userRest } = user;
+
+      // Level이 없는 경우 일반 user 값만 return
+      if (!userLevel) {
+        return userRest;
+      }
+
+      const {
+        id,
+        name,
+        imageUrl,
+        summary,
+        description,
+        userLevelDetail,
+        level,
+      } = userLevel;
+      const details = userLevelDetail.map(({ detail }) => detail);
+
+      return {
+        ...userRest,
+        userLevel: {
+          id,
+          name,
+          imageUrl,
+          summary,
+          description,
+          details,
+          level,
+        },
+      };
+    } catch (e) {
+      throw new HttpException(ERROR_MESSAGE.NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  updateUserLevel(user: User) {
+    try {
+      const { id: userId, userLevel } = user;
+
+      return { userId, userLevel };
+    } catch (e) {
+      throw new HttpException(ERROR_MESSAGE.NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  findUserCount() {}
+}
+
+@Module({
+  imports: [TransformDao],
+  controllers: [],
+  providers: [],
+  exports: [TransformDao],
+})
+export class TransformDaoModule {}
