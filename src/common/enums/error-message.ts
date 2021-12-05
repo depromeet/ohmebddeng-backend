f232126546decbd7277ebd348cee60f11c8136df
Sungmin Kim <kimsungmin.dev@gmail.com>
2021-12-05 21:17:12 +0900

    Refactor #75 food API Error Handling 추가 (#81)
    * feat: 🎸 error message
    
    ✅ Closes: #75
    
    * refactor: 💡 try-catch문을 사용하여 예외처리 진행
    
    ✅ Closes: #75
    
    * fix: 🐛 에러메세지가 제대로 안나오는 부분 수정
    
    ✅ Closes: #75
    
    * fix: 🐛 merge 충돌 error 제거
    
    ✅ Closes: #75
    
    * fix: 🐛 에러 메세지 변경
    
    ✅ Closes: #75
    
    * fix: 🐛 리뷰 음식 api 로직 수정
    
    ✅ Closes: #75
    
    * feat: 🎸 error message
    
    ✅ Closes: #75
    
    * refactor: 💡 try-catch문을 사용하여 예외처리 진행
    
    ✅ Closes: #75
    
    * fix: 🐛 에러 메세지 변경
    
    ✅ Closes: #75
    
    * refactor: 💡 Food API 1차 Refactoring
    
    ✅ Closes: #75
    
    * refactor: 💡 불필요한 코드 제거
    
    ✅ Closes: #75
    
    * refactor: 💡 food api 2차 리팩터링
    
    ✅ Closes: #75
    
    * fix: 🐛 에러 메세지 수정
    
    ✅ Closes: #75
    
    * refactor: 💡 imageUrl값이 null인 것은 제외 하도록 수정
    
    ✅ Closes: #75
    
    * fix: 🐛 에러메세지 수정..BAD_REQUEST -> NOT_FOUND
    
    ✅ Closes: #75

---
+++ src/common/enums/error-message.ts
@@ -0,0 +1,6 @@
+export enum ERROR_MESSAGE {
+  NOT_FOUND = '없는 페이지를 찾으셨군요.. 박수!',
+  INTERNAL_SERVER_ERROR = '펑! 서버가 터졌어요. 개발자가 빠르게 해결할거에요',
+  BAD_REQUEST = '찾는 정보가 없는 것 같습니다..',
+  UNAUTHORIZED = '레벨테스트가 필요해요!',
+}
